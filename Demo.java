7ba759e350365c2b52f1f91b6ffbee97be9da80c
Vaibhav Sutar <vsutar1010@gmail.com>
2026-08-10 19:33:59 +0530

    added one more print statement

--- Demo.java
+++ Demo.java
@@ -2,6 +2,8 @@ public class Demo{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-	System.out.println("shivpratap");
+	    System.out.println("shivpratap");
+
+		System.out.println("Hello, World!");
     }
 }
